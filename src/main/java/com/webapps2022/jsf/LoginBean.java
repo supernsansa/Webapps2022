1378b3b0150cdd921221532f7c05adde49e949c9
Anton N O Callens <supernsansa@gmail.com>
2022-04-11 22:45:53 +0200

    Implemented Lab 6 security functionality.

---
+++ src/main/java/com/webapps2022/jsf/LoginBean.java
@@ -0,0 +1,26 @@
+package com.webapps2022.jsf;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@Named
+@RequestScoped
+public class LoginBean implements Serializable {
+
+    public void logout() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try {
+            //this method will disassociate the principal from the session (effectively logging him/her out)
+            request.logout();
+            context.addMessage(null, new FacesMessage("User is logged out"));
+        } catch (ServletException e) {
+            context.addMessage(null, new FacesMessage("Logout failed."));
+        }
+    }
+}
