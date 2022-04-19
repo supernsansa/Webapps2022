b86bf72525f4289b7045d605681d91aa2d77f5af
Anton N O Callens <supernsansa@gmail.com>
2022-04-19 23:05:04 +0200

    Got the basics up. Login page now displays user's name and GBP account balance.

--- src/main/java/com/webapps2022/entity/SystemUser.java
+++ src/main/java/com/webapps2022/entity/SystemUser.java
@@ -1,56 +1,65 @@
 package com.webapps2022.entity;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Query;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 
 @Entity
 public class SystemUser implements Serializable {
 
+    /**
+     * @Id @GeneratedValue(strategy = GenerationType.AUTO) private Long id;
+     */
+    // here on could use Bean Validation annotations to enforce specific rules - this could be alternatively implemented when validating the form in the web tier
+    // for now we check only for Null values
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private String username;
 
     // here on could use Bean Validation annotations to enforce specific rules - this could be alternatively implemented when validating the form in the web tier
     // for now we check only for Null values
     @NotNull
-    String username;
+    private String userpassword;
 
-    // here on could use Bean Validation annotations to enforce specific rules - this could be alternatively implemented when validating the form in the web tier
-    // for now we check only for Null values
-    @NotNull
-    String userpassword;
+    //The amount of GBP the user is holding (cannot be negative)
+    @PositiveOrZero
+    private Double balance;
 
-/**
-    @NotNull
-    String name;
+    //Each user belongs to a single user group (user or admin)
+    private String systemUserGroup;
 
-    @NotNull
-    String surname;
-*/
+    //each user has many debit transaction records and each record belongs to one user
+    @OneToMany(cascade = CascadeType.ALL, targetEntity = Payment.class)
+    private List<Payment> payments;
 
     public SystemUser() {
     }
 
-    public SystemUser(String username, String userpassword) {
+    public SystemUser(String username, String userpassword, String systemUserGroup) {
         this.username = username;
         this.userpassword = userpassword;
+        this.balance = 2000.00;
+        this.systemUserGroup = systemUserGroup;
         //this.name = name;
         //this.surname = surname;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    /**
+     * public Long getId() { return id; }
+     *
+     * public void setId(Long id) { this.id = id; }
+     *
+     * @return
+     */
     public String getUsername() {
         return username;
     }
@@ -67,31 +76,46 @@ public class SystemUser implements Serializable {
         this.userpassword = userpassword;
     }
 
-/**
-    public String getName() {
-        return name;
+    public Double getBalance() {
+        return balance;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setBalance(Double balance) {
+        this.balance = balance;
     }
 
-    public String getSurname() {
-        return surname;
+    public String getSystemUserGroup() {
+        return systemUserGroup;
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
+    public void setSystemUserGroup(String systemUserGroup) {
+        this.systemUserGroup = systemUserGroup;
     }
-    */
 
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Payment> debits) {
+        this.payments = debits;
+    }
+
+    /**
+     * public String getName() { return name; }
+     *
+     * public void setName(String name) { this.name = name; }
+     *
+     * public String getSurname() { return surname; }
+     *
+     * public void setSurname(String surname) { this.surname = surname; }
+     */
     @Override
     public int hashCode() {
         int hash = 5;
-        hash = 97 * hash + Objects.hashCode(this.id);
+        //hash = 97 * hash + Objects.hashCode(this.id);
         hash = 97 * hash + Objects.hashCode(this.username);
         hash = 97 * hash + Objects.hashCode(this.userpassword);
-        //hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.balance);
         //hash = 97 * hash + Objects.hashCode(this.surname);
         return hash;
     }
@@ -105,13 +129,17 @@ public class SystemUser implements Serializable {
             return false;
         }
         final SystemUser other = (SystemUser) obj;
-        if (!Objects.equals(this.id, other.id)) {
-            return false;
-        }
+        /**
+         * if (!Objects.equals(this.id, other.id)) { return false; }
+         *
+         */
         if (!Objects.equals(this.userpassword, other.userpassword)) {
             return false;
         }
-        return Objects.equals(this.username, other.username); 
+        if (!Objects.equals(this.balance, other.balance)) {
+            return false;
+        }
+        return Objects.equals(this.username, other.username);
     }
 
 }
